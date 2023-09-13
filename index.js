a708617d81426d4c3eae7999b3f330909f5107f1
Jayesh Darji <jayesh.darji@zeuslearning.mumbai>
2023-09-13 10:28:43 +0530

    fourth commit

--- index.js
+++ index.js
@@ -3,12 +3,25 @@ const inp = document.querySelector("input");
 const para = document.querySelector("p");
 const head1 = document.querySelector("h1");
 
+
+console.log("First");
+
 btn.addEventListener("click", (e)=> {
     e.preventDefault();
-    console.log(inp.value);
+    console.log("Click");
     para.innerText = `My Name is ${inp.value}`;
 })
 
+console.log("Second");
+
+setTimeout(()   => {
+    console.log('After 1 second');
+}, 0);
+
+console.log("Third");
+
 inp.addEventListener("keyup",()=> {
+    console.log("Keyup");
     head1.innerText = `You added ${inp.value}`;
 })
+console.log("Fourth");
